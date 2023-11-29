0f8c66a21d50a8b84f60efff3ac2049c8a363772
Beatriz Pinheiro <70537307+beatrizpinheiro@users.noreply.github.com>
2023-11-29 13:07:01 -0300

    Update run.bash

--- ros-humble/run.bash
+++ ros-humble/run.bash
@@ -172,8 +172,8 @@ xhost + local:
 # DISPLAY passes the DISPLAY environment variable to the container, allowing it to connect to the X server
 
 docker run -it \
-  -e DISPLAY \                              
-  -e QT_X11_NO_MITSHM=1 \                   
+  -e DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
   -e XAUTHORITY=$XAUTH \
   -v "$XAUTH:$XAUTH" \
   -v "/tmp/.X11-unix:/tmp/.X11-unix" \
